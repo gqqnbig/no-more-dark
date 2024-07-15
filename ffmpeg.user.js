1ebbf729c7bed51c53be1d25b0237312e7b1ea20
QQ喵 <gqqnb2005@gmail.com>
2024-07-15 10:32:08 +0800

    Revert ffmpeg to light mode

---
+++ ffmpeg.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        No more dark
+// @namespace   gqqnbig
+// @match       *://*.ffmpeg.org/*
+// @grant       GM_addStyle
+// @version     1.0
+// @author      -
+// @description 2024/7/15 10:24:58
+// ==/UserScript==
+
+
+GM_addStyle(`
+body {
+  background-color: white  !important;
+  color: black !important;
+}
+`);
